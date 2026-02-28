c8905f92ab0bcfc92fef0b45a7ac46e378bf58e9
mitra-123 <152537213+mitra-123@users.noreply.github.com>
2026-02-27 22:10:01 -0500

    Upload dynamodb.js

--- backend/database/dynamodb.js
+++ backend/database/dynamodb.js
@@ -1,1 +1,13 @@
+import AWS from 'aws-sdk';
 
+AWS.config.update({
+    region: "us-east-2",
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+})
+
+const dynamoDB = new AWS.DynamoDB();
+
+export {
+    dynamoDB
+}
