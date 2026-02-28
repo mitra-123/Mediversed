29706ac20f6ff1500b4e21d79e2c387e2ed81765
mitra-123 <152537213+mitra-123@users.noreply.github.com>
2026-02-27 22:06:43 -0500

    Upload app.js and json files

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,33 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import account_routes from "./routes/account_route.js";
+import patient_routes from "./routes/patient_route.js";
+import nurse_routes from "./routes/nurse_route.js";
+import skill_routes from "./routes/skill_route.js";
+import cookieParser from 'cookie-parser';
 
+const app = express();
+
+const port = 9897;
+
+var corsOptions = {
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  credentials: true,
+  optionsSuccessStatus: 204
+}
+
+app.use(express.json());
+app.use(cors(corsOptions))
+app.use(cookieParser())
+app.use(account_routes)
+app.use(patient_routes)
+app.use(nurse_routes)
+app.use(skill_routes)
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+export default app;
